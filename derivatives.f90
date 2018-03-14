51a631e300e0cfe8e5b5cdc9d4432615966023b7
Olivier Asselin <oasselin@login2.stampede2.tacc.utexas.edu>
2018-03-14 18:36:21 -0500

    Correction as noticed in LA-inversion branch: initialize sumB with B_1 rather than 0

--- derivatives.f90
+++ derivatives.f90
@@ -1091,6 +1091,15 @@ MODULE derivatives
       sumBR = (0.D0,0.D0)
       sumBI = (0.D0,0.D0)
 
+      DO ikx=1,iktx
+         DO ikyp=1,iktyp
+            iky=ikyp+iktyp*mype
+            if(kh2/=0 .and. L(ikx,iky)==1 ) then
+               sumBR(ikx,ikyp) = BRkt(ikx,1,ikyp)
+               sumBI(ikx,ikyp) = BIkt(ikx,1,ikyp)
+            end if
+         end DO
+      end DO
 
       !Compute \tilde{A}, which is \hat{A} up to an arbitrary constant (\hat{A} at z = dz/2 set to 0)
       DO ikx=1,iktx
