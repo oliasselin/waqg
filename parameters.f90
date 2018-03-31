bc51fe88d3a4f92e5b6a85848eaeffbd5fe7661c
Olivier Asselin <oasselin@login4.stampede2.tacc.utexas.edu>
2018-03-31 17:43:13 -0500

    Tested F == 0 but linear == 0. Exponential growth of the small initial error on J(psi,q)~q, so q_t~q grows expo

--- parameters.f90
+++ parameters.f90
@@ -29,7 +29,7 @@ MODULE parameters
     !For comprehensive test only!
     !---------------------------!
 
-    integer, parameter :: init_test = 1
+    integer, parameter :: init_test = 0
     integer, parameter :: forcing = 0
 
     double precision, parameter :: a_x = 4., a_y = 4., a_z = 5., a_t = 0.
@@ -40,7 +40,7 @@ MODULE parameters
     !Tags to specify run!
     !-------------------!
     
-    integer, parameter :: linear=1                      !1: set the nonlinear terms (advection) to 0. 
+    integer, parameter :: linear=0                      !1: set the nonlinear terms (advection) to 0. 
     integer, parameter :: inviscid=0                    !1: No dissipation, otherwise: dissipation
     integer, parameter :: init_wageo=0                  !1: Initialize wk with Ro*wak
 
@@ -175,7 +175,7 @@ MODULE parameters
     integer :: iter
     integer :: itermax=1000000000
     real :: maxtime=1.                      
-    double precision, parameter :: delt=0.05*U_scale*dz/2    !0.0005*U_scale*dz                ! T_visc = 0.25D0*dz*dz/nu
+    double precision, parameter :: delt=0.05*U_scale*dz/2/2.    !0.0005*U_scale*dz                ! T_visc = 0.25D0*dz*dz/nu
     double precision, parameter :: gamma=0.!1e-2!4e-3!1e-2!7.e-3            !Robert filter parameter
 
     !Other successful viscosity: 5e-2 * (10./ktrunc_x ) **2. 
